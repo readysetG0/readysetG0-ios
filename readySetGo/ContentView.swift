921ff26ff6b778cbcb2be3221a9e850ac0dfbe90
Oneul1213 <toda1213@naver.com>
2024-08-31 23:47:51 +0900

    Initial Commit

---
+++ readySetGo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  readySetGo
+//
+//  Created by 임정상 on 2024/08/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ readySetGo/readySetGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  readySetGoApp.swift
+//  readySetGo
+//
+//  Created by 임정상 on 2024/08/31.
+//
+
+import SwiftUI
+
+@main
+struct readySetGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
